6b664c38fb0882f815db602452857cdc0298bc5a
Benzazoua <billalben252@gmail.com>
2024-05-05 21:12:38 +0100

    Filter portfolio & improve code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,23 +12,57 @@ const lightbox = GLightbox({
 });
 
 // BACK TO TOP BUTTON
+document.addEventListener("DOMContentLoaded", () => {
 const backToTop = document.querySelector(".back-to-top");
 window.addEventListener("scroll", () => {
   backToTop.classList[window.scrollY > 200 ? "add" : "remove"]("active");
 });
+});
 
 // Active nav link
-const navLinks = document.querySelectorAll(".nav-link");
-let lastActiveNavLink = navLinks[0];
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".nav-link");
+  let lastActiveNavLink = navLinks[0];
 
-navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", () => {
-    lastActiveNavLink.classList.remove("active");
-    navLink.classList.add("active");
-    lastActiveNavLink = navLink;
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", () => {
+      lastActiveNavLink.classList.remove("active");
+      navLink.classList.add("active");
+      lastActiveNavLink = navLink;
+    });
   });
 });
 
 // Dynamic copyright year
-const copyrightYear = document.querySelector("span.copyright-yr");
-copyrightYear.textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", () => {
+  const copyrightYear = document.querySelector("span.copyright-yr");
+  copyrightYear.textContent = new Date().getFullYear();
+});
+
+// Filters for portfolio
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll("[data-filter]");
+  const items = document.querySelectorAll("[data-category]");
+
+  let lastActiveButton = buttons[0];
+  lastActiveButton.classList.add("active");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      lastActiveButton.classList.remove("active");
+      lastActiveButton = button;
+      lastActiveButton.classList.add("active");
+
+      const category = this.getAttribute("data-filter");
+      filterItems(category);
+    });
+  });
+
+  function filterItems(category) {
+    items.forEach(function (item) {
+      const itemCategory = item.getAttribute("data-category");
+      item.style.display =
+        category === "all" || category === itemCategory ? "block" : "none";
+    });
+  }
+});
