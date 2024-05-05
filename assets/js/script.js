fd1119f51eba4851b894d7c742a95af26ce5716d
Benzazoua <billalben252@gmail.com>
2024-05-05 16:16:36 +0100

    improve code

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const lightbox = GLightbox({
+  touchNavigation: true,
+  href: "https://youtu.be/_QLF3B_fSJA?si=MAI3snuWnQ9u9w1c",
+  type: "video",
+  source: "youtube", //vimeo, youtube or local
+  width: 900,
+  autoPlayVideos: "true",
+});
+
+// BACK TO TOP BUTTON
+const backToTop = document.querySelector(".back-to-top");
+window.addEventListener("scroll", () => {
+  backToTop.classList[window.scrollY > 200 ? "add" : "remove"]("active");
+});
+
+// Active nav link
+const navLinks = document.querySelectorAll(".nav-link");
+let lastActiveNavLink = navLinks[0];
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    lastActiveNavLink.classList.remove("active");
+    navLink.classList.add("active");
+    lastActiveNavLink = navLink;
+  });
+});
